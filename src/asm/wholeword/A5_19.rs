25c176f246e5bf866522b537776a84ef07539bcb
ivario123 <ivar.jonsson18@gmail.com>
2024-02-20 12:11:37 +0100

    -> A5_19

--- src/asm/wholeword/A5_19.rs
+++ src/asm/wholeword/A5_19.rs
@@ -1,8 +1,9 @@
 use crate::asm::Mask;
 use crate::instruction;
 use crate::prelude::*;
-use arch::{wrapper_types::*, Register};
 use crate::ParseError;
+use crate::ToThumb;
+use arch::{wrapper_types::*, Register};
 use paste::paste;
 
 pub trait LocalTryInto<T> {
@@ -72,7 +73,8 @@ instruction!(
     },
     LdrshLiteral : {
         imm12   as u16  : Imm12     : 0 -> 11 try_into,
-        rt      as u8   : Register  : 12 -> 15 try_into
+        rt      as u8   : Register  : 12 -> 15 try_into,
+        u       as u8   : bool      : 23 -> 23 local_try_into
     },
     LdrshRegister : {
         rm      as u8   : Register  : 0 -> 3 try_into,
@@ -145,5 +147,98 @@ impl Parse for A5_19 {
         }
         // This should be unreachable
         Err(ParseError::Invalid32Bit("A5_19"))
+    }
+}
+
+impl ToThumb for A5_19 {
+    fn encoding_specific_operations(self) -> thumb::Thumb {
+        match self {
+            Self::LdrhLiteral(el) => thumb::LdrhLiteral::builder()
+                .set_rt(el.rt)
+                .set_add(Some(el.u))
+                .set_imm(el.imm12.into())
+                .complete()
+                .into(),
+            Self::LdrhImmediateT2(el) => thumb::LdrhImmediate::builder()
+                .set_w(Some(false))
+                .set_add(Some(true))
+                .set_index(Some(true))
+                .set_rt(el.rt)
+                .set_rn(el.rn)
+                .set_imm(el.imm12.into())
+                .complete()
+                .into(),
+            Self::LdrhImmediateT3(el) => thumb::LdrhImmediate::builder()
+                .set_w(Some(el.w))
+                .set_add(Some(el.u))
+                .set_index(Some(el.p))
+                .set_rt(el.rt)
+                .set_rn(el.rn)
+                .set_imm(el.imm8 as u32)
+                .complete()
+                .into(),
+            Self::LdrhRegister(el) => {
+                let shift = match ImmShift::try_from((Shift::Lsl, el.imm2.into())) {
+                    Ok(el) => Some(el),
+                    _ => None,
+                };
+                thumb::LdrhRegister::builder()
+                    .set_rt(el.rt)
+                    .set_rn(el.rn)
+                    .set_rm(el.rm)
+                    .set_shift(shift)
+                    .complete()
+                    .into()
+            }
+            Self::LdrshImmediateT1(el) => thumb::LdrshImmediate::builder()
+                .set_add(true)
+                .set_index(true)
+                .set_wback(false)
+                .set_rt(el.rt)
+                .set_rn(el.rn)
+                .set_imm(Some(el.imm12.into()))
+                .complete()
+                .into(),
+            Self::LdrshImmediateT2(el) => thumb::LdrshImmediate::builder()
+                .set_add(el.u)
+                .set_index(el.p)
+                .set_wback(el.w)
+                .set_rt(el.rt)
+                .set_rn(el.rn)
+                .set_imm(Some(el.imm8 as u32))
+                .complete()
+                .into(),
+            Self::LdrshLiteral(el) => thumb::LdrshLiteral::builder()
+                .set_add(el.u)
+                .set_rt(el.rt)
+                .set_imm(el.imm12.into())
+                .complete()
+                .into(),
+            Self::LdrshRegister(el) => {
+                let shift = match ImmShift::try_from((Shift::Lsl, el.imm2.into())) {
+                    Ok(el) => Some(el),
+                    _ => None,
+                };
+                thumb::LdrshRegister::builder()
+                    .set_rt(el.rt)
+                    .set_rn(el.rn)
+                    .set_rm(el.rm)
+                    .set_shift(shift)
+                    .complete()
+                    .into()
+            }
+            Self::Ldrsht(el) => thumb::Ldrsht::builder()
+                .set_rt(el.rt)
+                .set_rn(el.rn)
+                .set_imm(Some(el.imm8 as u32))
+                .complete()
+                .into(),
+            Self::Ldrht(el) => thumb::Ldrht::builder()
+                .set_rt(el.rt)
+                .set_rn(el.rn)
+                .set_imm(Some(el.imm8 as u32))
+                .complete()
+                .into(),
+        }
     }
 }
